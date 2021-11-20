4943f722d710a130d0ce70bc752dc768b8648785
AndreyFlik <89208286+AndreyFlik@users.noreply.github.com>
2021-11-20 13:39:48 +0000

    Deploying to gh-pages from @ AndreyFlik/goit-js-hw-10@ff033ffab6e435e5e7944fe8d19ed7f37f85ff9f 🚀

--- src.0dcbb10c.js
+++ src.0dcbb10c.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./css/styles.css");const s=300;
 },{"./css/styles.css":"krre"}]},{},["Focm"], null)
-//# sourceMappingURL=/parcel-project-template/src.0dcbb10c.js.map
+//# sourceMappingURL=/goit-js-hw-10/src.0dcbb10c.js.map
