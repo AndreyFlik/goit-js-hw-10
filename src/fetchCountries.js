b921a9ebaed042c6d878966e3cb9e47a010d673c
Andrey Yasentiuk <Andrey.dj.flik@gmail.com>
2021-11-20 23:54:48 +0200

    работает без рефакторинга

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,1 @@
-// function fetchCountries(currentCoutry) {
-//   fetch(`https://restcountries.com/v3.1/name/${currentCoutry.trim()}`)
-//     .then(response => {
-//       return response.json();
-//     })
-//     .then(country => {
-//       console.log(country);
-//     })
-//     .catch(error => {
-//       console.log(error);
-//     });
-// }
+// export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import './css/styles.css';
+// import { fetchCountries } from './fetchCountries.js';
+
 import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
-// const COUNTRY_URL = 'https://restcountries.com/v3.1/name/';
 const searchInput = document.querySelector('#search-box');
 const countryMarkup = document.querySelector('.country-info');
 const countryList = document.querySelector('.country-list');
@@ -18,13 +19,27 @@ function inputEvent(event) {
 function fetchCountries(currentCoutry) {
   return fetch(`https://restcountries.com/v3.1/name/${currentCoutry.trim()}`)
     .then(response => {
-      return response.json();
+      //   console.log(response.json());
+      if (response.ok) {
+        return response.json();
+      }
     })
     .then(country => {
-      renderCountry(country);
+      console.log(country);
+      if (country.length > 10) {
+        countryMarkup.innerHTML = '';
+        countryList.innerHTML = '';
+        return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+      } else {
+        renderCountry(country);
+      }
     })
     .catch(error => {
-      console.log(error);
+      countryMarkup.innerHTML = '';
+      countryList.innerHTML = '';
+      if (currentCoutry != 0) {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
     });
 }
 
@@ -49,7 +64,6 @@ function renderCountry(country) {
       .join('');
     countryList.innerHTML = '';
     countryMarkup.innerHTML = markup;
-    // countryMarkup.insertAdjacentHTML('beforeend', markup);
   } else {
     const markup = country
       .map(countries => {
@@ -58,7 +72,6 @@ function renderCountry(country) {
 `;
       })
       .join('');
-    //   countryList.insertAdjacentHTML('beforeend', markup);
     countryList.innerHTML = markup;
     countryMarkup.innerHTML = '';
   }
